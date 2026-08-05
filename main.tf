d10d64c39df06455793b15f094520c16a83b2838
Graham Nguyen <graham.n@itcgroup.io>
2026-08-05 14:11:08 +0700

    initial testing

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-test-bucket-12345"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Example security group for testing scanners"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
